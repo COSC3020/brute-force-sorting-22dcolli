f7802552b93393641beaa7be10fa79f26468c43c
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-05-05 16:15:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,41 +1,42 @@
+
 // Used replit AI tool on one logic issue marked below
 function findSortedPermutation(a, currPos, permutations) {
     if (currPos === a.length - 1) {
-      return isSorted(a); 
+        //replit help me put my count in the right place
+        permutations.count++;
+        return isSorted(a); 
     }
+    //needed help with the logic inside this loop too
     for (let i = currPos; i < a.length; i++) {
-      swap(a, i, currPos); 
-      permutations.count++;
-      if (findSortedPermutation(a, currPos + 1, permutations)) {
-        return true; 
-      }
-      swap(a, currPos, i);
+        swap(a, i, currPos); 
+        if (findSortedPermutation(a, currPos + 1, permutations)) {
+            return true; 
+        }
+        swap(a, currPos, i);
     }
-    // Reset permutations count when backtracking
-    permutations.count -= (a.length - currPos); 
     return false; 
 }
 
 function swap(a, first, second) {
-  const tmp = a[first];
-  a[first] = a[second];
-  a[second] = tmp;
+    const tmp = a[first];
+    a[first] = a[second];
+    a[second] = tmp;
 }
 
 function isSorted(a) {
-  for (let i = 0; i < a.length - 1; i++) {
-    if (a[i] > a[i + 1]) {
-      return false; 
+    for (let i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            return false; 
+        }
     }
-  }
-  return true; 
+    return true; 
 }
 
 function permutationSort(a) {
-  const permutations = { count: 0 }; 
-  findSortedPermutation(a, 0, permutations);
-  console.log("Permutations tried:", permutations.count);
+    const permutations = { count: 0 }; 
+    findSortedPermutation(a, 0, permutations);
+    //console.log("tried:", permutations.count);
 }
 
-//const arrtest = [3,2,1];
+//const arrtest = [3, 2, 1];
 //permutationSort(arrtest);
