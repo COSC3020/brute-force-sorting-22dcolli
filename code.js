3db31d4ebd2a942c4f7fab214455b15be5db4f63
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-05-05 16:28:58 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,41 +1,39 @@
-function findSortedPermutation(a, currPos, permutations) {
-    if (currPos === a.length - 1) {
-        permutations.count++; // Increment the count before checking if the array is sorted
-        return isSorted(a); 
+function findSortedPermutation(arr, currPos, permutations) {
+    if (currPos === arr.length - 1) {
+        permutations.count++;
+        return isSorted(arr); 
     }
-    for (let i = currPos; i < a.length; i++) {
-        swap(a, i, currPos); 
-        if (findSortedPermutation(a, currPos + 1, permutations)) {
+    for (let i = currPos; i < arr.length; i++) {
+        swap(arr, i, currPos); 
+        if (findSortedPermutation(arr, currPos + 1, permutations)) {
             return true; 
         }
-        swap(a, currPos, i);
+        swap(arr, currPos, i);
     }
     return false; 
 }
 
-function swap(a, first, second) {
-    const tmp = a[first];
-    a[first] = a[second];
-    a[second] = tmp;
+function swap(arr, first, second) {
+    const tmp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = tmp;
 }
 
-function isSorted(a) {
-    for (let i = 0; i < a.length - 1; i++) {
-        if (a[i] > a[i + 1]) {
+function isSorted(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
             return false; 
         }
     }
     return true; 
 }
 
-function permutationSort(a) {
+function permutationSort(arr) {
     const permutations = { count: 0 }; 
-    findSortedPermutation(a, 0, permutations);
-    //console.log("Permutations tried:", permutations.count);
+    findSortedPermutation(arr, 0, permutations);
+    return permutations.count;
 }
 
-//const arrtest = [5,4, 3, 2, 1];
-//permutationSort(arrtest);
 
 //const arrtest = [3, 2, 1];
 //permutationSort(arrtest);
